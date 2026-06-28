1a35e296bb0d003f5e0e11f946a00fff06868491
Srivathsan Ram <sri5617@gmail.com>
2026-06-29 00:18:31 +0800

    adding functionality to database.go

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -1,8 +1,33 @@
 package database
 
+import (
+	"os"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/driver/postgres"
+	"gorm.io/gorm"
+)
+
 type Database struct {
+	DB *gorm.DB
 }
 
 func GetDB() (*Database, error) {
-	return &Database{}, nil
+	databaseURL := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect to database")
+	}
+
+	// Set connection pool settings
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get SQL db instance")
+	}
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	return &Database{DB: db}, nil
 }
