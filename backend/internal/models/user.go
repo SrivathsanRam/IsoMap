5535969655fb2605d5b3d911b28f46748e62ef22
Srivathsan Ram <sri5617@gmail.com>
2026-06-29 00:26:50 +0800

    user model

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -1,12 +1,23 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"github.com/google/uuid"
+
+
+)
 
 type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID   uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Name string    `json:"name" gorm:"not null"`
+	Email string    `json:"email" gorm:"uniqueIndex"`
+	PasswordHash string    `json:"-" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	
 }
 
 func (user *User) Greet() string {
-	return fmt.Sprintf("Hello, I am %s", user.Name)
+	return fmt.Sprintf("Hello, Welcome %s", user.Name)
 }
